efa0940084b7363d27ab6ea3967b3cdb73562bf6
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-17 01:10:52 -0300

    Requisito 1 refatorado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,39 +1,15 @@
 const data = require('../data/zoo_data');
 
-// const getSpeciesByIds = (...ids) => {
-//   const especies = [];
-//   if (ids === undefined) {
-//     return especies;
-//   }
-//   const {species} = data;
-//   const result = ids.forEach((elemento) => {
-//     console.log(elemento);
-//     species.filter((element) => element.id === elemento)
-    
-//   })
-//   console.log(elemento);
-//   return result;
-// }
+const { species } = data;
 
 const getSpeciesByIds = (...ids) => {
-  const {species} = data;
-  const listaDeEspecies = [];
-  if (ids === undefined) {
-    return listaDeEspecies;
-  }
- for (let i = 0; i < ids.length; i += 1) {
-   for (let i2 = 0; i2 < species.length; i2 += 1) {
-     if (ids[i] === species[i2].id) {
-      listaDeEspecies.push(species[i2])
-     }
-   }
- }
- return listaDeEspecies
-}
+  const getSpecies = ids.map((elementMap) => {
+    const specieFind = species.find((elementFind) => elementFind.id === elementMap);
+    return specieFind;
+  });
+  return getSpecies;
+};
 
-console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
-
-// const result = species.filter((element) => element.id === ids);
-// console.log();
