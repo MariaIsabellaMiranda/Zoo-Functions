0de62463745b5236b069d2562a678894dfcb1e2b
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-17 17:18:43 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const { employees } = data;
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const isManager = (id) => employees.some((pessoa) => pessoa.managers.includes(id));
+
+const getRelatedEmployees = (managerId) => {
+  const gerente = isManager(managerId);
+  if (gerente === true) {
+    const gerenciados = employees.filter((pessoas) => pessoas.managers.includes(managerId));
+    return gerenciados.map((element) => `${element.firstName} ${element.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+};
 
 module.exports = { isManager, getRelatedEmployees };
