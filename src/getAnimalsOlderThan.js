94de9233dea9919abc992ab5ad3060e5ce19ce0e
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-16 22:35:08 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 const getAnimalsOlderThan = (animal, age) => {
-  const idades = data.species.forEach((especie) => {
-    especie.age
-  })
-  // every((age) => data.species.animal)
-  console.log(idades);
-}
-getAnimalsOlderThan()
+  const Objspecie = species.find((element) => element.name === animal);
+  const { residents } = Objspecie;
+  const animais = residents.every((element) => element.age >= age);
+  return animais;
+};
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
