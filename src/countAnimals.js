f6f42ea4431b6d1a2f92b78f110f9ecdaa02a7ae
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-18 02:05:42 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
+
+const pegaEsSex = (obj) => {
+  const { specie, sex } = obj;
+  const nomeEspecie = species.find((nome) => nome.name === specie);
+  const { residents } = nomeEspecie;
+  const pegaPorSex = residents.filter((animais) => animais.sex === sex);
+  return pegaPorSex.length;
+};
+
+const pegaEspecie = (es) => {
+  const { specie } = es;
+  const nomeEspecie = species.find((nome) => nome.name === specie);
+  const { residents } = nomeEspecie;
+  return residents.length;
+};
+
+const semArgumento = () => {
+  const newObject = {};
+  species.forEach((especies) => { newObject[especies.name] = especies.residents.length; });
+  return newObject;
+};
+
+const countAnimals = (animal) => {
+  if (!animal) {
+    return semArgumento();
+  }
+  const valor = Object.values(animal);
+  if (valor.length === 1) {
+    return pegaEspecie(animal);
+  }
+  return pegaEsSex(animal);
+};
 
 module.exports = countAnimals;
