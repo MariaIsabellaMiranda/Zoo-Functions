113d4eab995f270c45aa9af048d4308bfce7b8a8
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-18 17:47:28 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,25 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const { prices } = data;
 
-function calculateEntry(entrants) {
-  // seu código aqui
+const countEntrants = (entrants) => {
+  const child = entrants.filter((visitante) => visitante.age < 18);
+  const adult = entrants.filter((visitante) => visitante.age >= 18 && visitante.age < 50);
+  const senior = entrants.filter((visitante) => visitante.age >= 50);
+  const idades = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return idades;
+};
 
-}
+const calculateEntry = (entrants) => {
+  if (!entrants) { return 0; }
+  if (Object.keys(entrants).length === 0) { return 0; }
+  const { child, adult, senior } = countEntrants(entrants);
+  const valor = (prices.child * child) + (prices.adult * adult) + (prices.senior * senior);
+  return valor;
+};
 
 module.exports = { calculateEntry, countEntrants };
