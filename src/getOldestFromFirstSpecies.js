18a0c4c644d1e82f2096b237b35910c1a692f698
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-21 20:34:52 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getEspecie = (id) => {
+  let especie = '';
+  employees.forEach((element) => {
+    if (element.id === id) {
+      const [firstEspecie] = element.responsibleFor;
+      especie = species.filter((element2) => element2.id === firstEspecie);
+    }
+  });
+  return especie[0].residents;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let animal = getEspecie(id).map((element) => element.age);
+  animal = Math.max(...animal);
+  animal = getEspecie(id).find((element) => element.age === animal);
+  animal = Object.values(animal);
+  return animal;
 }
 
 module.exports = getOldestFromFirstSpecies;
