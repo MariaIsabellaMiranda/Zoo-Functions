79bee75777e09ac6193540e694d435837e5d6da9
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-22 03:19:59 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,60 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species } = data;
+
+const getPessoa = (id) => {
+  let pessoa = '';
+  const [nome] = Object.values(id);
+  pessoa = employees.find((element) => Object.values(element).includes(nome));
+  return pessoa;
+};
+
+const getLocation = (id) => {
+  const locais = [];
+  const { responsibleFor } = getPessoa(id);
+  responsibleFor.forEach((elementMap) => {
+    const localEspecie = species.find((elementSpecie) => elementMap === elementSpecie.id);
+    locais.push(localEspecie.location);
+  });
+  return locais;
+};
+
+const getNames = (id) => {
+  const nomes = [];
+  const { responsibleFor } = getPessoa(id);
+  responsibleFor.forEach((elementMap) => {
+    const localEspecie = species.find((elementSpecie) => elementMap === elementSpecie.id);
+    nomes.push(localEspecie.name);
+  });
+  return nomes;
+};
+
+const criaObjetoFinal = (nome) => {
+  const { id, firstName, lastName } = getPessoa(nome);
+  const objPessoa = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getNames(nome),
+    locations: getLocation(nome),
+  };
+  return objPessoa;
+};
+
+const todosFuncionarios = () => {
+  const megaArray = [];
+  const arrayId = [];
+  employees.forEach((elemntFor) => { arrayId.push({ id: elemntFor.id }); });
+  arrayId.forEach((elemento) => {
+    megaArray.push(criaObjetoFinal(elemento));
+  });
+  return megaArray;
+};
+
+function getEmployeesCoverage(id) {
+  if (!id) { return todosFuncionarios(); }
+  if (getPessoa(id) === undefined) { throw new Error('Informações inválidas'); }
+  if (id) { return criaObjetoFinal(id); }
 }
 
 module.exports = getEmployeesCoverage;
